0736a71f9a6c8ea9184ebb8f8c5a546d3e731167
LinhChi-tapcode <klinhnguyen2204@gmail.com>
2024-12-06 21:44:18 +0700

    Add files via upload

---
+++ Mini-pro 2.1/Câu 2/Cau2.js
@@ -0,0 +1,13 @@
+const Character = {
+    Name: "Miles Morales",
+    age: 18,
+    role: "Anonymous Hero",
+    planet: "Earth 1610",
+}   
+const [Name, age, role, planet] = ["Miles Morales", 18, "Anonymous Hero", "Earth 1610" ];
+
+console.log(`Tên nhân vật: ${Name}`);
+console.log(`Tuổi: ${age}`);
+console.log(`Vai trò: ${role}`);
+console.log(`Hành tinh: ${planet}`);
+
